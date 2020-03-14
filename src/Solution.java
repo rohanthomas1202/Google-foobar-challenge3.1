413eebcd87f005a62a513730dca908cf27e24354
rohanthomas1202 <rst170000@utdallas.edu>
2020-03-14 03:13:07 -0500

    Final bug fixes

--- src/Solution.java
+++ src/Solution.java
@@ -1,84 +1,45 @@
-import java.util.ArrayList;
-
 public class Solution {
     public static void main(String[] args) {
-
-
-        System.out.println(solution(0, 3));
-
+        System.out.println(solution(17,  2000000000));
     }
 
 
+
+    // Solution which computes the checksum in O(N) time
+    // Efficient way to find XOR of consecutive numbers
     public static int solution(int start, int length) {
-        int check_sum;
-        //check_sum = workers_ID(start, length);
-        check_sum = XOR_checksum(start, length);
-        return check_sum;
-
-    }
-
-    /*An inefficient way to solve the question,
-     * first attempt
-     */
-    public static int workers_ID(int start, int length) {
-        int worker_list = 0;
-        boolean check = true;
-        for (int rows = 0; rows < length; rows++) {
-            for (int columns = 0; (columns < length + 1); columns++) {
-
-                if (columns + rows == length) {
-                    check = false;
-                } else if (check) {
-
-                    worker_list = worker_list ^ start;
-
-                    start++;
-                } else {
-                    start++;
-                }
-            }
-            check = true;
-        }
-        return worker_list;
-    }
-
-    public static int XOR_checksum(int start, int length) {
-        int check_sum = 0;
-        int from = -1;
-        int to = -1;
-
+        int check_sum = 0;              // keeps  track of the check_sum
+        int from;                       // first number in the row
+        int to;                         // last valid number to compute XOR of in the row
+        // for each row, find the first and last valid worker ID to compute the check_sum
         for (int curr_row = 0; curr_row < length; curr_row++) {
+            // starts at the first element in each row
             from = (start + (curr_row * length));
+            // ends at one less than the previous row
             to = from + (length - 1 - curr_row);
+            // if the first and last elements are the same, computing last row
             if (from != to) {
-                System.out.println("From: " + from + " To: " + to);
                 check_sum = check_sum ^ (XOR_from_to((from - 1), to));
             } else {
-                System.out.println("Endln: " + from);
+                // XOR the last value to the current check_sum
                 check_sum = check_sum ^ (from);
             }
         }
         return check_sum;
     }
 
-    // find the XOR value of a number from a given number to a given number
+    // find the XOR value of a consecutive numbers
     public static int XOR_from_to(int from, int to) {
-        int val = XOR_1_to_n(from) ^ XOR_1_to_n(to);
-        System.out.println(val);
-
-        return (val);
-
+        // Finding XOR of every element from a to b
+        // (a ^ ... ^ b) = (1 ^ ... ^ b) ^ (1 ^ ... ^ a)
+        return (XOR_1_to_n(from) ^ XOR_1_to_n(to));
     }
 
     // finding the XOR of 1 to a number efficiently
     public static int XOR_1_to_n(int num) {
         /*
-        The XOR of an even number followed by an odd(must be consecutive even and odd) will always yield 1
-        Using this, since we start at 1, the next digit is 2 followed by 3, which yields to just 1,
-        hence we can get all one's until we reach the number we are looking for
-        eg: XOR_1_to_n(5);  5 % 4 = 1, hence we must get 1 as XOR between 1 and 5
-        1 ^ ((2 ^ 3) --> 1) ^ ((4 ^ 5) --> 1)
-        1 ^ 1 ^ 1 = 0 ^ 1 = 1
+        The XOR of an even number followed by a consecutive odd will always yield 1
+        Using this, we can formulate a simple algorithm to compute the XOR of 1 to n in O(1)
         */
 
         int XOR_val = 0;
@@ -97,10 +58,7 @@ public class Solution {
                 XOR_val = 0;
                 break;
         }
-
-        System.out.println("1 to " + num + " : " + XOR_val);
         return XOR_val;
-
     }
 
 }
